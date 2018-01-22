e10dd3cd126eaf202772d6edca4f4af161092e91
Joel Purra <mig@joelpurra.se>
2018-01-22 15:17:23 +0100

    Enable babel transformation of es2015+ features

--- rollup.config.babel.js
+++ rollup.config.babel.js
@@ -18,30 +18,9 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-import json from "rollup-plugin-json";
+import babel from "rollup-plugin-babel";
 
-import babel from "./rollup.config.babel.js";
-import uglify from "./rollup.config.uglify.js";
-import filesize from "./rollup.config.filesize.js";
-import license from "./rollup.config.license.js";
-
-const inputName = "index";
-const fileExtension = ".js";
-const fileName = `${inputName}${fileExtension}`;
-
-export default {
-    plugins: [
-        json(),
-        babel(),
-        uglify(),
-        license(inputName),
-        filesize(),
-    ],
-    input: `${fileName}`,
-    output: {
-        format: "cjs",
-        sourcemap: true,
-        name: inputName,
-        file: `dist/${fileName}`,
-    },
-};
+export default () =>
+    babel({
+        exclude: "node_modules/**",
+    });
